2124fba0eb0085be1ae047338b65f06d35161dd9
Louis GERARD <dhawos@proton.me>
2023-08-02 23:41:14 +0200

    feat(logger): add logger when server starts

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
-use actix_web::{web, App, HttpServer, Responder};
-use stores::{Store, postgres::PostgresStore};
+use std::env;
 
+use actix_web::{web, App, HttpServer, Responder, middleware};
+use env_logger::Env;
+use stores::{Store, postgres::PostgresStore};
+use log::info;
 mod db_schema;
 mod stores;
 
@@ -16,17 +19,24 @@ async fn list_games<T: Store + Clone>(data: web::Data<AppState<T>>) -> impl Resp
 
 #[actix_web::main]
 pub async fn start_server() -> std::io::Result<()> {
+    let mut builder = env_logger::Builder::from_env(
+        Env::default().default_filter_or("info")
+    );
+    builder
+        .target(env_logger::Target::Stdout)
+        .init();
     let store = PostgresStore::new();
-    let state = AppState{store: store};
+    let state = AppState{store};
         
     HttpServer::new(move || {
         App::new()
+            .wrap(middleware::Logger::default())
             .app_data(web::Data::new(state.clone()))
             .service(
                 web::scope("/games").route("", web::get().to(list_games::<PostgresStore>))
             )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 8081))?
     .run()
     .await
 }
--- src/stores/postgres.rs
+++ src/stores/postgres.rs
@@ -1,7 +1,6 @@
 use diesel::prelude::*;
 use dotenvy::dotenv;
 use std::env;
-use config::{Config,Environment};
 use crate::db_schema::schema::boardgames::dsl::*;
 use crate::db_schema::types::BoardGame;
 use super::Store;
@@ -21,19 +20,18 @@ impl PostgresStore {
         let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
         let manager = ConnectionManager::<PgConnection>::new(database_url);
         let pool = Pool::builder().build(manager).expect("Failed to create pool.");
-        PostgresStore{pool: pool}
+        PostgresStore{pool}
     }
 }
 
 impl Store for PostgresStore {
     fn select_boardgames(&self) -> Vec<BoardGame> {
         let mut conn = self.pool.get().unwrap();
-        let results = boardgames
+        boardgames
             .limit(5)
             .select(BoardGame::as_select())
             .load(&mut conn)
-            .expect("Error loading posts");
-        return results;
+            .expect("Error loading posts")
     }
 }
 
